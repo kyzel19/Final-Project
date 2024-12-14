e96b56c959d25174864dd1251906648f7c9c53cc
kyzel19 <kyzelroperez20@gmail.com>
2024-12-15 03:47:01 +0800

    changes

---
+++ Activity5.py
@@ -0,0 +1,6 @@
+
+print("Fahrenheit to Celcius")
+temp=eval(input('\nEnter Temperature in Fahrenheit: '))
+celsius=(temp - 32) * 5/9
+print(f'\n\nThe conversion of {temp} degrees Fahrenheit is {celsius} degrees Celsius\n\nor')
+print(f'\nThe conversion of {temp} degrees Fahrenheit is {round(celsius, 2)} degrees Celsius')
